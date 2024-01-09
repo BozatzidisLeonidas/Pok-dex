b0f6295eee388e8e9f5765b4bd3cf13244df6990
Leonidas Bozatzidis <bozatzidisleonidas@gmail.com>
2024-01-09 13:27:11 +0200

    first commit

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navigation.css';
+import Seen from './seen.png';
+import Caught from './caught.png';
+
+const Navigation = () => {
+    return(
+    <div className='title'>
+        <div className='titleLeft'>
+            <p>Pokédex</p>
+            <div className='caught-seen' >
+                <div className='caught'>
+                    <img src={Caught} alt='pokeball_icon_caught' style={{width: "25px"}}></img>
+                    <p>186</p>
+                </div>
+                <div className='seen'>
+                    <img src={Seen} alt='pokeball_icon_seen' style={{width: "25px"}}></img>
+                    <p>431</p>
+                </div>
+            </div>
+        </div>
+    </div>
+ );
+}
+
+export default Navigation;
